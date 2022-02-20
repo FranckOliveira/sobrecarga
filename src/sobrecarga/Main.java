83490f9da778c56ca7971d1568f7ae3a68ad247a
Francisco Oliveira <gleriston.silva61@gmail.com>
2022-02-20 14:21:49 -0300

    Teste com sobrecarga

--- src/sobrecarga/Main.java
+++ src/sobrecarga/Main.java
@@ -4,9 +4,22 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d,5d);
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("quadrado: "+ areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5d,5d);
+        System.out.println("retangulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Trapezio: "+areaTrapezio);
+
+        float areaLosango = Quadrilatero.area(5f,5f);
+        System.out.println("Losango: " + areaLosango);
+
+
+        /*Quadrilatero.area(5d,5d);
         Quadrilatero.area(7,8,9);
-        Quadrilatero.area(5f,5f);
+        Quadrilatero.area(5f,5f);*/
     }
 }
